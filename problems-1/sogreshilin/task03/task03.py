a48fdb17bb51c5b341dcfdc5d26ed9c8c25c0c4e
Alexander <sogreshilin.ridder@gmail.com>
2018-03-11 20:33:48 +0700

    add test to check complementary set is not prime

--- problems-1/sogreshilin/task03/task03.py
+++ problems-1/sogreshilin/task03/task03.py
@@ -5,6 +5,9 @@ import unittest
 
 
 def is_prime(n):
+    if n < 2:
+        return False
+
     last_factor = int(math.sqrt(n))
     return all(n % i for i in range(2, last_factor + 1))
 
@@ -50,7 +53,6 @@ def measure_time(func, *args, **kwargs):
 
 class TestEratosthenes(unittest.TestCase):
     small_n = 10_000
-    big_n = 10_000_000
 
     def test_simple(self):
         self.assertEqual(eratosthenes_list(3), [2])
@@ -69,20 +71,31 @@ class TestEratosthenes(unittest.TestCase):
         with self.assertRaises(TypeError):
             eratosthenes_list("string_value")
 
-    def test_time(self):
-        list_time = measure_time(eratosthenes_list, self.big_n)
-        set_time = measure_time(eratosthenes_set, self.big_n)
-        bitarray_time = measure_time(eratosthenes_bitarray, self.big_n)
-        print()
-        print("Time test results:")
-        print(" - list : {0:.4f}".format(list_time))
-        print(" - set  : {0:.4f}".format(set_time))
-        print(" - bits : {0:.4f}".format(bitarray_time))
+    def check_complementary_number_set(self, primes_factory, upper_bound):
+        for i in range(upper_bound):
+            if i in primes_factory(upper_bound):
+                self.assertTrue(is_prime(i))
+            else:
+                self.assertFalse(is_prime(i))
+
+    def test_check_complementary_set_of_numbers(self):
+        upper_bound = 1000
+        self.check_complementary_number_set(eratosthenes_list, upper_bound)
+        self.check_complementary_number_set(eratosthenes_set, upper_bound)
+        self.check_complementary_number_set(eratosthenes_bitarray, upper_bound)
 
 
-def main():
-    unittest.main()
+def test_time():
+    n = 10_000_000
+    list_time = measure_time(eratosthenes_list, n)
+    set_time = measure_time(eratosthenes_set, n)
+    bitarray_time = measure_time(eratosthenes_bitarray, n)
+    print("Time test results:")
+    print(" - list : {0:.4f}".format(list_time))
+    print(" - set  : {0:.4f}".format(set_time))
+    print(" - bits : {0:.4f}".format(bitarray_time))
 
 
 if __name__ == "__main__":
-    main()
+    test_time()
+    unittest.main()
